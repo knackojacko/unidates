8413698f0e8ea3b3e6d3dbd883a2cbadf0aa4f34
Pranay Panday <s3555246@student.rmit.edu.au>
2017-08-25 03:54:52 +0000

    Layout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,11 +9,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "ExampleUser",
-                                         email: "s3555241@student.rmit.edu.au",gender: "Man",preference: "Women", dob:"1994-09-01",
+                                         email: "s3555241@student.rmit.edu.au",gender: "Male",preference: "Female", dob:"1994-09-01",
                                          password:"123456789",
                                          password_confirmation: "123456789" } }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
+  
+  
+  
   
 end
