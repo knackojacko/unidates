a09aaf8e60d4d95cdd11b23e69b9f432e124a554
Alaaden Kanj <s3539548@student.rmit.edu.au>
2017-08-25 13:54:10 +0000

    Working matching page

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -18,6 +18,7 @@ class QuestionnairesController < ApplicationController
 
 
   def edit
+  
   end
 
 
@@ -35,65 +36,15 @@ class QuestionnairesController < ApplicationController
 
 
   def update
-
-  end
-
-
-  def destroy
-
-  end
-
-  private
-
-    def set_questionnaire
-      @questionnaire = Questionnaire.find(params[:id])
-    end
-
-
-    def questionnaire_params
-      params.require(:questionnaire).permit(:user_id, :q1, :q2, :q3, :q4, :q5, :q6, :q7, :q8, :q9, :q10)
-    end
-end
-class QuestionnairesController < ApplicationController
-  before_action :set_questionnaire, only: [:show, :edit, :update, :destroy]
-
-
-  def index
-    @questionnaire = Questionnaire.all
-  end
-
-
-  def show
-    @questionnaire = Questionnaire.find(params[:id])    
-  end
-
-
-  def new
-    @questionnaire = Questionnaire.new
-  end
-
-
-  def edit
-  end
-
-
-  def create
-
-    @questionnaire = Questionnaire.new(questionnaire_params)
-    
-    if @questionnaire.save
-      redirect_to @questionnaire
+    @questionnaire = Questionnaire.find(params[:id])
+    if @questionnaire.update_attributes(questionnaire_params)
+      flash[:success] = "preference updated"
+       render 'index'
+       
     else
-      render 'new'
+      render 'edit'
     end
-
   end
-
-
-  def update
-
-  end
-
 
   def destroy
 
