f164f9dded1efde8b8dff2a44d8747077891b8dc
Jack Douma <jack.w.t.douma@gmail.com>
2017-09-08 10:38:11 +0000

    Final fix

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -59,7 +59,7 @@ class ReportsController < ApplicationController
   private
   
     def report_params
-      params.require(:report).permit(:user_id, :reason, :reported_user_id)
+      params.require(:report).permit(:user_id, :problem, :reported_user_id)
     end
   
 end
