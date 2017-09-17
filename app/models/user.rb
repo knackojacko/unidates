69d9efe94a141c74c3d9571265bfdb63f5108bfa
Alaaden Kanj <s3539548@student.rmit.edu.au>
2017-09-17 03:51:25 +0000

    heroku fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,6 +61,7 @@ class User < ApplicationRecord
 
 
     private
+      
       def confirmation_token
         if self.confirm_token.blank?
           self.confirm_token = SecureRandom.urlsafe_base64.to_s
