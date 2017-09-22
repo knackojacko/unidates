7bcc4cce7cf127bdb2c549606509a775c2081b30
Danyon <s3547463@student.rmit.edu.au>
2017-09-22 23:56:25 +1000

    Added chat page with name bar and message pane

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,32 +1,52 @@
 class ConversationsController < ApplicationController
-  def create
-    @conversation = Conversation.get(current_user.id, params[:user_id])
-    
-    add_to_conversations unless conversated?
- 
-    respond_to do |format|
-      format.js
-    end
-  end
+    def create
 
-  def close
-    @conversation = Conversation.find(params[:id])
- 
-    session[:conversations].delete(@conversation.id)
- 
-    respond_to do |format|
-      format.js
+        # Close all other conversations
+        # @conversations = Conversation.all
+        # @conversations.each do |conversation|
+        #     @conversation = Conversation.find(conversation[:id])
+        #
+        #     session[:conversations].delete(@conversation.id)
+        #     respond_to do |format|
+        #         format.js
+        #     end
+        # end
+
+        # Used for debugging conversation
+        request.session.each {|key, value|
+            if key.to_s == "conversations"
+                puts key.to_s + " --> " + value.to_s
+            end
+        }
+
+        # Create new conversation
+        @conversation = Conversation.get(current_user.id, params[:user_id])
+        add_to_conversations unless conversated?
+
+        respond_to do |format|
+            format.js
+        end
+
     end
-  end
- 
-  private
- 
-  def add_to_conversations
-    session[:conversations] ||= []
-    session[:conversations] << @conversation.id
-  end
- 
-  def conversated?
-    session[:conversations].include?(@conversation.id)
-  end
+
+    def close
+        @conversation = Conversation.find(params[:id])
+
+        session[:conversations].delete(@conversation.id)
+
+        respond_to do |format|
+            format.js
+        end
+    end
+
+    private
+
+    def add_to_conversations
+        session[:conversations] ||= []
+        session[:conversations] << @conversation.id
+    end
+
+    def conversated?
+        session[:conversations].include?(@conversation.id)
+    end
 end
