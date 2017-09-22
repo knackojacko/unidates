49528e43f2e0f21ecb7e01dc885aa0fa4d11c6ed
Danyon <s3547463@student.rmit.edu.au>
2017-09-23 00:28:38 +1000

    Fixed bug where user can see chat without signing in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,107 +18,101 @@
 //= require_tree .
 
 //START OF FILE
-$( document ).on('turbolinks:load', function() {
-  
-  
-/* ------------------------ START: APPLICATION WIDE ------------------------- */
+$(document).on('turbolinks:load', function () {
+
+
+    /* ------------------------ START: APPLICATION WIDE ------------------------- */
 // Login success popup
-  setTimeout(function(){
-      $('.alert').fadeOut();
-  }, 3000);
-/* ------------------------- END: APPLICATION WIDE -------------------------- */
+    setTimeout(function () {
+        $('.alert').fadeOut();
+    }, 3000);
+    /* ------------------------- END: APPLICATION WIDE -------------------------- */
 
 
-
-
-/* -------------------------- START: PROFILE PAGE --------------------------- */
-    $("#profileImage").click(function(e) {
+    /* -------------------------- START: PROFILE PAGE --------------------------- */
+    $("#profileImage").click(function (e) {
         $("#imageUpload").click();
     });
-    
-    function fasterPreview( uploader ) {
-        if ( uploader.files && uploader.files[0] ){
-              $('#profileImage').attr('src', 
-                 window.URL.createObjectURL(uploader.files[0]) );
+
+    function fasterPreview(uploader) {
+        if (uploader.files && uploader.files[0]) {
+            $('#profileImage').attr('src',
+                window.URL.createObjectURL(uploader.files[0]));
         }
     }
-    
-    $("#imageUpload").change(function(){
-        fasterPreview( this );
-    }); 
-/* --------------------------- END: PROFILE PAGE ---------------------------- */
 
-
-
-
-/* ------------------------ START: PREFERENCES PAGE ------------------------- */
-    // CYCLE IMAGE 1 FUNCTION
-      $imageArrayLeft = ["/cat.png", "/beach.png", "/movies.png", "/restaurant.png", "/video_games.png", "/android.png", "/art.png", "/paris.png", "/bar.png", "/escalators.png"];
-      $arrayIndexLeft = 1;
-      $imageArrayRight = ["/dog.png", "/park.png", "/books.png", "/cafe.png", "/board_games.png", "/iphone.png", "/sport.png", "/bali.png", "/club.png", "/elevators.png"];
-      $arrayIndexRight = 1;
-    
-      $("#image1").on({
-        mouseenter: function() {
-          $(this).css({ "border": "1px solid red", "border-radius": "200px" });
-        },
-        mouseleave: function() {
-          $(this).css("border", "");
-        },
-        click: function() {
-          $(this).delay(400).fadeOut(200, function() {
-            $(this).attr("src", $imageArrayLeft[$arrayIndexLeft]).fadeIn(200);
-            ++$arrayIndexLeft;
-          });
-          $("#image2").delay(400).fadeOut(200, function() {
-            $(this).attr("src", $imageArrayRight[$arrayIndexRight]).fadeIn(200);
-            ++$arrayIndexRight;
-          });
-        }
-      });
-    
-      // CYCLE IMAGE 2 FUNCTION
-      $("#image2").on({
-        mouseenter: function() {
-          $(this).css({ "border": "1px solid red", "border-radius": "200px" });
-        },
-        mouseleave: function() {
-          $(this).css("border", "");
-        },
-        click: function() {
-          $(this).delay(400).fadeOut(200, function() {
-            $(this).attr("src", $imageArrayRight[$arrayIndexRight]).fadeIn(200);
-            ++$arrayIndexRight;
-          });
-          $("#image1").delay(400).fadeOut(200, function() {
-            $(this).attr("src", $imageArrayLeft[$arrayIndexLeft]).fadeIn(200);
-            ++$arrayIndexLeft;
-          });
-        }
-      });
-/* ------------------------- END: PREFERENCES PAGE -------------------------- */
-
-
-
-
-/* --------------------------- START: CHATS PAGE ---------------------------- */
-  // Minimising chat window
-  (function() {
-    $(document).on('click', '.toggle-window', function(e) {
-      e.preventDefault();
-      var panel = $(this).parent().parent();
-      var messages_list = panel.find('.messages-list');
-   
-      panel.find('.panel-body').toggle();
-      panel.attr('class', 'panel panel-default');
-   
-      if (panel.find('.panel-body').is(':visible')) {
-        var height = messages_list[0].scrollHeight;
-        messages_list.scrollTop(height);
-      }
+    $("#imageUpload").change(function () {
+        fasterPreview(this);
     });
-  })();
-/* ---------------------------- END: CHATS PAGE ----------------------------- */
+    /* --------------------------- END: PROFILE PAGE ---------------------------- */
+
+
+    /* ------------------------ START: PREFERENCES PAGE ------------------------- */
+    // CYCLE IMAGE 1 FUNCTION
+    $imageArrayLeft = ["/cat.png", "/beach.png", "/movies.png", "/restaurant.png", "/video_games.png", "/android.png", "/art.png", "/paris.png", "/bar.png", "/escalators.png"];
+    $arrayIndexLeft = 1;
+    $imageArrayRight = ["/dog.png", "/park.png", "/books.png", "/cafe.png", "/board_games.png", "/iphone.png", "/sport.png", "/bali.png", "/club.png", "/elevators.png"];
+    $arrayIndexRight = 1;
+
+    $("#image1").on({
+        mouseenter: function () {
+            $(this).css({"border": "1px solid red", "border-radius": "200px"});
+        },
+        mouseleave: function () {
+            $(this).css("border", "");
+        },
+        click: function () {
+            $(this).delay(400).fadeOut(200, function () {
+                $(this).attr("src", $imageArrayLeft[$arrayIndexLeft]).fadeIn(200);
+                ++$arrayIndexLeft;
+            });
+            $("#image2").delay(400).fadeOut(200, function () {
+                $(this).attr("src", $imageArrayRight[$arrayIndexRight]).fadeIn(200);
+                ++$arrayIndexRight;
+            });
+        }
+    });
+
+    // CYCLE IMAGE 2 FUNCTION
+    $("#image2").on({
+        mouseenter: function () {
+            $(this).css({"border": "1px solid red", "border-radius": "200px"});
+        },
+        mouseleave: function () {
+            $(this).css("border", "");
+        },
+        click: function () {
+            $(this).delay(400).fadeOut(200, function () {
+                $(this).attr("src", $imageArrayRight[$arrayIndexRight]).fadeIn(200);
+                ++$arrayIndexRight;
+            });
+            $("#image1").delay(400).fadeOut(200, function () {
+                $(this).attr("src", $imageArrayLeft[$arrayIndexLeft]).fadeIn(200);
+                ++$arrayIndexLeft;
+            });
+        }
+    });
+    /* ------------------------- END: PREFERENCES PAGE -------------------------- */
+
+
+    /* --------------------------- START: CHATS PAGE ---------------------------- */
+    // Minimising chat window
+    (function () {
+        $(document).on('click', '.toggle-window', function (e) {
+            e.preventDefault();
+            var panel = $(this).parent().parent();
+            var messages_list = panel.find('.messages-list');
+
+            panel.find('.panel-body').toggle();
+            panel.attr('class', 'panel panel-default');
+
+            if (panel.find('.panel-body').is(':visible')) {
+                var height = messages_list[0].scrollHeight;
+                messages_list.scrollTop(height);
+            }
+        });
+    })();
+    /* ---------------------------- END: CHATS PAGE ----------------------------- */
 
 
 // END OF FILE
