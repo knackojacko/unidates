b449fb77fbc3d9768cbbe4d3991b4bf942622e75
Jack Douma <jack.w.t.douma@gmail.com>
2017-10-13 05:31:35 +0000

    Validation for user sign up (First iteration). Decision: User must be older than 18. User must be younger than 100. User must upload a profile photo. Profile photo must be an image.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,52 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token, :reset_token
-    before_create :confirmation_token
-    attr_accessor :remember_token    
-    VALID_EMAIL_REGEX = /\As\d{7}@student\.rmit\.edu\.au\z/i
-    has_many :questionnaire
-    has_many :report
-    has_many :like
-    has_many :matche
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_create :confirmation_token
+  attr_accessor :remember_token    
+  VALID_EMAIL_REGEX = /\As\d{7}@student\.rmit\.edu\.au\z/i
+  has_many :questionnaire
+  has_many :report
+  has_many :like
+  has_many :matche
 
-    has_attached_file :avatar, styles: {
-      square: '122x122#',
-      icon: '23x23#'
-    }
-    serialize :liked_users
-    has_secure_password
-    before_save {self.email = email.downcase}
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: {case_sensitive: false}
-    validates :dob, presence: true
-    validates :gender, presence: true
-    validates :preference, presence: true
+  has_attached_file :avatar, styles: {
+    square: '122x122#',
+    icon: '23x23#'
+  }
+  
+  serialize :liked_users
+  has_secure_password
+  before_save {self.email = email.downcase}
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :dob, presence: true
+  validates :gender, presence: true
+  # Call func to check if user under 18
+  validate :under_18
+  # Call func to check if user under maximum age
+  validate :over_max
+  validates :preference, presence: true
 
-    # Updated validation to skip password validation on update (as there is no password to validate)
-    validates :password, presence: true, confirmation: true, length: {in: 8..35}, on: :create
-    validates :password, length: {in: 8..35}, on: :update, allow_blank: true
-    validates :password_confirmation, presence: true, on: :create
-    validates :password_confirmation, presence: true, on: :update, allow_blank: true
-    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  # Updated validation to skip password validation on update (as there is no password to validate)
+  validates :password, presence: true, confirmation: true, length: {in: 8..35}, on: :create
+  validates :password, length: {in: 8..35}, on: :update, allow_blank: true
+  validates :password_confirmation, presence: true, on: :create
+  validates :password_confirmation, presence: true, on: :update, allow_blank: true
+  validates_attachment_content_type :avatar, presence: true, :content_type => /\Aimage\/.*\Z/
 
 
+  # Function to check if user under 18
+  def under_18
+    if dob + 18.years > Date.today
+      errors.add(:dob, "Sorry, you must be at least 18 years of age to use this site.")
+    end
+  end
+  
+  # Function to check if user over maximum age of 100
+  def over_max
+    if dob + 100.years < Date.today
+      errors.add(:dob, "Sorry, you must be less than 100 years of age to use this site.")
+    end
+  end
   
   class << self
     # Returns the hash digest of the given string.
@@ -44,37 +62,36 @@ class User < ApplicationRecord
     end
   end
     
-    # Returns a random token.
-    def User.new_token
-        SecureRandom.urlsafe_base64
-    end
+  # Returns a random token.
+  def User.new_token
+      SecureRandom.urlsafe_base64
+  end
 
-    def self.new_token
-        SecureRandom.urlsafe_base64
-    end
-    
-    def authenticated?(attribute, token)
-      digest = send("#{attribute}_digest")
-      return false if digest.nil?
-      BCrypt::Password.new(digest).is_password?(token)
-    end
+  def self.new_token
+      SecureRandom.urlsafe_base64
+  end
   
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
-    def remember
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
-    end
-    
-    # Forgets a user.
-    def forget
-        update_attribute(:remember_digest, nil)
-    end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Forgets a user.
+  def forget
+      update_attribute(:remember_digest, nil)
+  end
 
-    def email_activate
-      self.email_confirmed = true
-      self.confirm_token = nil
-      save!(:validate => false)
-    end
+  def email_activate
+    self.email_confirmed = true
+    self.confirm_token = nil
+    save!(:validate => false)
+  end
 
   # Sets the password reset attributes.
   def create_reset_digest
@@ -93,11 +110,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-    private
+  private
       
-      def confirmation_token
-        if self.confirm_token.blank?
-          self.confirm_token = SecureRandom.urlsafe_base64.to_s
-        end
-      end
+  def confirmation_token
+    if self.confirm_token.blank?
+      self.confirm_token = SecureRandom.urlsafe_base64.to_s
+    end
+  end
 end
